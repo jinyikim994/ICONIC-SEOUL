05834274cd27f7cb16ee767dc667dcad55b144b9
jinyikim <https://github.com/jinyikim994>
2025-09-05 17:12:10 +0900

    production page 추가

--- js/common.js
+++ js/common.js
@@ -3,3 +3,12 @@ function toggleMenu() {
     const nav = document.getElementById("nav-menu");
     nav.classList.toggle("show");
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+    const toggleButton = document.querySelector(".menu-toggle");
+    const menu = document.querySelector(".menu");
+
+    toggleButton.addEventListener("click", () => {
+        menu.classList.toggle("active");
+    });
+});
